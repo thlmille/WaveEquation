244bf02ed852bc93b6679323454c77b054807921
Thomas Miller <thlmille@ucsc.edu>
2012-03-17 11:28:20 -0700

    Pretty much working, can't really tell if correct though

--- WaveSolver.m
+++ WaveSolver.m
@@ -5,37 +5,39 @@
 %   This file uses a finite difference numerical method to
 %     solve the wave equation in one dimension.
 %
-L = pi;
-time = .01;
+L = 10;
+time = 5;
 c = 1;
 
-tstep = time/512;
-Lstep = L/512;
+tstep = time/256;
+Lstep = L/256;
 
 tvals = [0:tstep:time];
 xvals = [0:Lstep:L];
 
 vecsize = size(xvals);
-veclength = vecsize(2);
+xveclength = vecsize(2);
+vecsize = size(tvals);
+tveclength = vecsize(2);
 
-u = zeros(veclength, veclength);
+u = zeros(xveclength, tveclength);
 
 %
 % Fill in boundaries
 %
 % u(0, 0) -> u(L, 0)
-for i = 1:veclength
+for i = 1:xveclength
     u(i, 1) = f(xvals(i));
 end
 
 % u(0, 0) -> u(0, time)
-for i = 1:veclength
+for i = 1:tveclength
    u(1, i) = A(tvals(i)); 
 end
 
 % u(L, 0) -> u(L, time)
-for i = 1:veclength
-   u(veclength, i) = A(tvals(i));
+for i = 1:tveclength
+   u(xveclength, i) = A(tvals(i));
 end
 
 
@@ -43,8 +45,8 @@ end
 % Use finite difference method to fill u matrix that represents
 %   solution u(x, t)
 %
-for j = 2:veclength-1
-    for k = 2:veclength-1
+for k = 2:tveclength-1
+    for j = 2:xveclength-1
         numer = c^2*u(j+1, k-1) - 2*u(j, k-1) + u(j-1, k-1);        
         u(j, k) = tstep^2 * numer/(Lstep^2) + 2*u(j, k-1) - u(j-1, k-1);
     end
@@ -52,18 +54,19 @@ end
 
 %mesh (xvals, tvals, u)
 
-%plot(xvals, u(1:veclength, 200))
+%plot(xvals, u(1:veclength, 1))
 
-
-for k = 1:10:veclength
-    plot(xvals, u(1:veclength, k));
+h = 1;
+for k = 1:10:tveclength
+    plot(xvals, u(1:xveclength, k));
     title (num2str(tvals(k)))
     %axis([0, pi, -2, 2])
-    M(k) = getframe;
+    M(h) = getframe;
+    h = h + 1;
 end
 
 clf
-movie(M,1)
+movie(M, 3, 10)
 
 
 
